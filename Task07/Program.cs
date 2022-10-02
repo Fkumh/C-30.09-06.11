2e31e64857200c8e37d8e28ef5129724c25255b7
Ivan <Iv.Zavyalov07@gmail.com>
2022-10-02 11:46:12 +0500

    Add Task05 and Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+int lastDigit = num % 10;
+Console.WriteLine($"Последняя цифра числа {num} -> {lastDigit}");
+}
+else 
+Console.WriteLine("Число не трёхзначное!");
