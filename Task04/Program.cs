9637ab2c106d5fb64b37f4ae3716143247115a9b
Ivan <Iv.Zavyalov07@gmail.com>
2022-10-02 14:03:47 +0500

    Add homework

---
+++ Task04/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22 
+
+Console.Write("Введите 1-ое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) 
+{
+    if (num1 > num3) Console.WriteLine($"Больше число: {num1}"); 
+    else Console.WriteLine($"Больше число: {num3}");
+}
+else 
+{
+    if (num2 > num3) Console.WriteLine($"Больше число: {num2}"); 
+    else Console.WriteLine($"Больше число: {num3}");
+}
---
+++ Task08/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число (число не должно быть отрицательным): ");
+int num = Convert.ToInt32(Console.ReadLine());
+int counted = 1;
+
+if (num >= 0)
+{
+while (counted <= num)
+{
+    if (counted % 2 == 0) Console.Write($"{counted} ");
+    counted++;
+};
+}
+else Console.WriteLine("Число отрицательное!");
