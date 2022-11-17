ea1e0f5a04af7fb92b571965ff262d3ab58d82a0
Ivan <Iv.Zavyalov07@gmail.com>
2022-11-17 21:18:29 +0500

    Homework

---
+++ Homework13.11/HW02/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите целое число:");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число:");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+int result = default;
+
+void NaturalNumbers(int num1, int num2, int res)
+{
+    if (num1 == num2)
+    {
+        res += num1;
+    }
+    if (num1 < num2)
+    {
+        res += num1;
+        NaturalNumbers(num1 + 1, num2, res);
+    }
+    else Console.WriteLine(res);
+}
+if (numb1 > numb2) NaturalNumbers(numb2, numb1, result);
+else NaturalNumbers(numb1, numb2, result);
+
---
+++ Homework13.11/HW03/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите целое число m:");
+long numb1 = Convert.ToInt64(Console.ReadLine());
+Console.WriteLine("Введите целое число n:");
+long numb2 = Convert.ToInt64(Console.ReadLine());
+
+long AkkermanFunc(long num1, long num2)
+{
+    if (num1 == 0)
+    {
+        return num2 + 1;
+    }
+    if (num1 > 0 && num2 == 0)
+    {
+        return AkkermanFunc(num1 - 1, 1);
+    }
+    return AkkermanFunc(num1 - 1, AkkermanFunc(num1, num2 - 1));
+}
+Console.WriteLine(AkkermanFunc(numb1, numb2));
